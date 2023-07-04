e8efafa64e98fe9857f99f0ac8b8a95cf101dd83
Jose Cuenca <jcuenca@unsa.edu.pe>
2023-07-04 12:22:43 -0500

    corrigiendo service

--- service-worker.js
+++ service-worker.js
@@ -1,20 +1,50 @@
-var GHPATH =  '/ia-final' ;
-var APP_PREFIX =  'ifpwa_' ;
-var VERSION =  'version_00' ;
+let cacheName = "FakeYapeDetector";// 👈 any unique name
 
-const staticDevIa = "dev-ia-final-v1"
-const assets = [
-  `${GHPATH}/`, 
-  `${GHPATH}/index.html`, 
-  `${GHPATH}/css/styles.css`, 
-  `${GHPATH} /css/normalize.css`,
-  `${GHPATH}/img/yape.png`, 
-]
+let filesToCache = [
+  "/ia-final/", // 👈 your repository name , both slash are important
+  "service-worker.js",
+  "css/normalize.css",
+  "css/styles.css",
+  "img/yape.png",
+  "manifest.json"
+  // add your assets here
+];
 
-self.addEventListener("install", installEvent => {
-  installEvent.waitUntil(
-    caches.open(staticDevIa).then(cache => {
-      cache.addAll(assets)
+self.addEventListener("install", function (event) {
+  event.waitUntil(caches.open(cacheName).then((cache) => {
+    console.log('installed successfully')
+    return cache.addAll(filesToCache);
+  }));
+});
+
+self.addEventListener('fetch', function (event) {
+
+  if (event.request.url.includes('clean-cache')) {
+    caches.delete(cacheName);
+    console.log('Cache cleared')
+  }
+
+  event.respondWith(caches.match(event.request).then(function (response) {
+    if (response) {
+      console.log('served form cache')
+    } else {
+      console.log('Not serving from cache ', event.request.url)
+    }
+    return response || fetch(event.request);
+  })
+  );
+});
+
+self.addEventListener('activate', function (e) {
+  e.waitUntil(
+    caches.keys().then(function (keyList) {
+      return Promise.all(keyList.map(function (key) {
+        if (key !== cacheName) {
+          console.log('service worker: Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
     })
-  )
-})
+  );
+  return self.clients.claim();
+});
